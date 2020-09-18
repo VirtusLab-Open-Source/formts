759b4cc407673a0a7fe142418672185526962800
Mikołaj Klaman <mixvar@gmail.com>
2020-09-18 23:13:49 +0200

    chore: project setup (#1)
    * chore: create package json and ts configs
    
    * chore: setup linting
    
    * chore: setup tests using jest
    
    * chore: setup commitlint

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { useFormts } from "./index";
+
+describe("useFormts", () => {
+  it("should not be implemented yet", () => {
+    expect(() => useFormts()).toThrowError("not implemented!");
+  });
+});
