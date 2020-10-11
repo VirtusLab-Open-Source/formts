0d8b3c5c0feed29a6607f5b2bf4d0e8b4de02eb1
Viktoriya Bilovus <vbilovus@Vikas-MacBook-Pro-2.local>
2020-10-11 23:09:13 +0200

    feat: create-form-validator draft

--- src/core/builders/create-form-validator.ts
+++ src/core/builders/create-form-validator.ts
@@ -1,9 +1,13 @@
+import { isFalsy } from "../../utils";
+import { FieldDescriptor } from "../types/field-descriptor";
 import { FormSchema } from "../types/form-schema";
 import {
   FieldValidator,
   FormValidator,
   ValidateFn,
+  ValidationTrigger,
 } from "../types/form-validator";
+import { impl } from "../types/type-mapper-util";
 
 /**
  * Create form validator based on provided set of validation rules.
@@ -35,9 +39,72 @@ import {
  */
 export const createFormValidator = <Values extends object, Err>(
   _schema: FormSchema<Values, Err>,
-  _builder: (
+  builder: (
     validate: ValidateFn
   ) => Array<FieldValidator<unknown, Err, unknown[]>>
 ): FormValidator<Values, Err> => {
-  return {} as any;
+  const validators = builder(validate).reduce((acc, x) => {
+    (acc as any)[impl(x.field).path] = x;
+    return acc;
+  }, {} as Record<string, FieldValidator<unknown, Err, unknown[]>>);
+
+  const shouldFireValidation = (
+    descriptor: FieldDescriptor<unknown, Err>,
+    trigger?: ValidationTrigger
+  ): boolean => {
+    if (!trigger) {
+      return true;
+    } else {
+      const fieldValidator = validators[impl(descriptor).path];
+      return fieldValidator
+        ? fieldValidator.triggers
+          ? fieldValidator.triggers.includes(trigger)
+          : true
+        : false;
+    }
+  };
+
+  const formValidator: FormValidator<Values, Err> = {
+    validate: (fields, getValue, trigger) => {
+      const fieldsToValidate = fields.filter(x =>
+        shouldFireValidation(x, trigger)
+      );
+
+      return Promise.all(
+        fieldsToValidate.map(async field => {
+          const fieldValidator = validators[impl(field).path]!;
+          const value = getValue(field);
+          const error = await runValidationForField(fieldValidator, value);
+
+          return { field, error };
+        })
+      );
+    },
+  };
+
+  return formValidator;
+};
+
+const validate: ValidateFn = config => ({
+  type: "inner",
+  field: config.field,
+  triggers: config.triggers,
+  validators: config.rules,
+  dependencies: config.dependencies,
+});
+
+const runValidationForField = async <Value, Err>(
+  validator: FieldValidator<Value, Err, unknown[]>,
+  value: Value
+): Promise<Err | null> => {
+  const rules = validator.validators([] as any);
+  for (const rule of rules) {
+    if (!isFalsy(rule)) {
+      const result = await rule(value);
+      if (result !== null) {
+        return result;
+      }
+    }
+  }
+  return null;
 };
--- src/core/types/form-validator.ts
+++ src/core/types/form-validator.ts
@@ -9,17 +9,11 @@ import { FieldDescriptor } from "./field-descriptor";
  *
  * @returns validation error of type `Err`, or `null` when field is valid
  */
-export type Validator<T, Err> = Validator.Sync<T, Err>;
-// | Validator.Async<T, Err>;
+export type Validator<T, Err> = ValidatorSync<T, Err> | ValidatorAsync<T, Err>;
 
-export namespace Validator {
-  export type Sync<T, Err> = {
-    (value: T): Err | null;
-  };
-  export type Async<T, Err> = {
-    (value: T): Promise<Err | null>;
-  };
-}
+export type ValidatorSync<T, Err> = (value: T) => Err | null;
+
+export type ValidatorAsync<T, Err> = (value: T) => Promise<Err | null>;
 
 export type ValidationTrigger = "change" | "blur" | "submit";
 
@@ -28,19 +22,21 @@ export type FormValidator<Values extends object, Err> = {
     fields: Array<FieldDescriptor<unknown, Err>>,
     getValue: <P>(field: FieldDescriptor<P, Err>) => P,
     trigger?: ValidationTrigger
-  ) => Array<{ field: FieldDescriptor<unknown, Err>; error: Err | null }>;
+  ) => Promise<
+    Array<{ field: FieldDescriptor<unknown, Err>; error: Err | null }>
+  >;
 };
 
 export type FieldValidator<T, Err, Dependencies extends any[]> = {
   type: "inner" | "outer";
   field: FieldDescriptor<T, Err>;
-  validators: (...deps: [...Dependencies]) => Array<Falsy | Validator<T, Err>>;
   triggers?: Array<ValidationTrigger>;
+  validators: (...deps: [...Dependencies]) => Array<Falsy | Validator<T, Err>>;
   dependencies?: readonly [...FieldDescTuple<Dependencies>];
 };
 
 export type ValidateFn = {
-  each: ValidateEachFn;
+  // each: ValidateEachFn;
 
   <T, Err, Dependencies extends any[]>(config: {
     field: FieldDescriptor<T, Err>;
