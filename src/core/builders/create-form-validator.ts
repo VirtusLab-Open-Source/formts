6a3059b40386c404197877a2a34bf6bb692f412f
pidkopajo <61269871+pidkopajo@users.noreply.github.com>
2020-10-19 14:29:28 +0200

    feat(core): create form validator (#19)

--- src/core/builders/create-form-validator.ts
+++ src/core/builders/create-form-validator.ts
@@ -1,9 +1,14 @@
+import { isFalsy } from "../../utils";
+import { FieldDescriptor } from "../types/field-descriptor";
 import { FormSchema } from "../types/form-schema";
 import {
   FieldValidator,
   FormValidator,
   ValidateFn,
+  ValidationTrigger,
+  Validator,
 } from "../types/form-validator";
+import { impl } from "../types/type-mapper-util";
 
 /**
  * Create form validator based on provided set of validation rules.
@@ -35,9 +40,100 @@ import {
  */
 export const createFormValidator = <Values extends object, Err>(
   _schema: FormSchema<Values, Err>,
-  _builder: (
-    validate: ValidateFn
-  ) => Array<FieldValidator<unknown, Err, unknown[]>>
+  builder: (validate: ValidateFn) => Array<FieldValidator<any, Err, any[]>>
 ): FormValidator<Values, Err> => {
-  return {} as any;
+  const allValidators = builder(validate);
+
+  const getValidatorsForField = (
+    descriptor: FieldDescriptor<unknown, Err>,
+    trigger?: ValidationTrigger
+  ): FieldValidator<any, Err, any[]>[] => {
+    const path = impl(descriptor).path;
+    const rootArrayPath = getRootArrayPath(path);
+
+    return allValidators.filter(x => {
+      const xPath = impl(x.field).path;
+      const isFieldMatch = x.type === "field" && xPath === path;
+      const isEachMatch = x.type === "each" && xPath === rootArrayPath;
+      const triggerMatches =
+        trigger && x.triggers ? x.triggers.includes(trigger) : true;
+
+      return triggerMatches && (isFieldMatch || isEachMatch);
+    });
+  };
+
+  const formValidator: FormValidator<Values, Err> = {
+    validate: (fields, getValue, trigger) => {
+      const fieldsToValidate = fields
+        .map(field => ({
+          field,
+          validators: getValidatorsForField(field, trigger),
+        }))
+        .filter(x => x.validators.length > 0);
+
+      return Promise.all(
+        fieldsToValidate.map(async ({ field, validators }) => {
+          const value = getValue(field);
+          const error = await firstNonNullPromise(validators, x =>
+            runValidationForField(x, value)
+          );
+
+          return { field, error };
+        })
+      );
+    },
+  };
+
+  return formValidator;
+};
+
+const validate: ValidateFn = config => ({
+  type: "field",
+  field: config.field,
+  triggers: config.triggers,
+  validators: config.rules,
+  dependencies: config.dependencies,
+});
+
+validate.each = config => ({
+  type: "each",
+  field: config.field as any,
+  triggers: config.triggers,
+  validators: config.rules,
+  dependencies: config.dependencies,
+});
+
+const runValidationForField = async <Value, Err>(
+  validator: FieldValidator<Value, Err, unknown[]>,
+  value: Value
+): Promise<Err | null> => {
+  const rules = validator
+    .validators([] as any)
+    .filter(x => !isFalsy(x)) as Validator<Value, Err>[];
+
+  return firstNonNullPromise(rules, async rule => await rule(value));
+};
+
+const firstNonNullPromise = async <T, V>(
+  list: T[],
+  mapper: (x: T) => Promise<V | null>
+): Promise<V | null> => {
+  for (const x of list) {
+    const result = await mapper(x);
+    if (result != null) {
+      return result;
+    }
+  }
+  return null;
+};
+
+// TODO rethink
+const getRootArrayPath = (path: string): string | undefined => {
+  const isArrayElement = path.lastIndexOf("]") === path.length - 1;
+  if (!isArrayElement) {
+    return undefined;
+  } else {
+    const indexStart = path.lastIndexOf("[");
+    return path.slice(0, indexStart);
+  }
 };
--- src/core/types/form-validator.ts
+++ src/core/types/form-validator.ts
@@ -9,17 +9,11 @@ import { FieldDescriptor } from "./field-descriptor";
  *
  * @returns validation error of type `Err`, or `null` when field is valid
  */
-export type Validator<T, Err> = Validator.Sync<T, Err>;
-// | Validator.Async<T, Err>;
+export type Validator<T, Err> = ValidatorSync<T, Err> | ValidatorAsync<T, Err>;
 
-export namespace Validator {
-  export type Sync<T, Err> = {
-    (value: T): Err | null;
-  };
-  export type Async<T, Err> = {
-    (value: T): Promise<Err | null>;
-  };
-}
+export type ValidatorSync<T, Err> = (value: T) => Err | null;
+
+export type ValidatorAsync<T, Err> = (value: T) => Promise<Err | null>;
 
 export type ValidationTrigger = "change" | "blur" | "submit";
 
@@ -28,14 +22,16 @@ export type FormValidator<Values extends object, Err> = {
     fields: Array<FieldDescriptor<unknown, Err>>,
     getValue: <P>(field: FieldDescriptor<P, Err>) => P,
     trigger?: ValidationTrigger
-  ) => Array<{ field: FieldDescriptor<unknown, Err>; error: Err | null }>;
+  ) => Promise<
+    Array<{ field: FieldDescriptor<unknown, Err>; error: Err | null }>
+  >;
 };
 
 export type FieldValidator<T, Err, Dependencies extends any[]> = {
-  type: "inner" | "outer";
+  type: "field" | "each";
   field: FieldDescriptor<T, Err>;
-  validators: (...deps: [...Dependencies]) => Array<Falsy | Validator<T, Err>>;
   triggers?: Array<ValidationTrigger>;
+  validators: (...deps: [...Dependencies]) => Array<Falsy | Validator<T, Err>>;
   dependencies?: readonly [...FieldDescTuple<Dependencies>];
 };
 
@@ -55,7 +51,7 @@ export type ValidateEachFn = <T, Err, Dependencies extends any[]>(config: {
   triggers?: ValidationTrigger[];
   dependencies?: readonly [...FieldDescTuple<Dependencies>];
   rules: (...deps: [...Dependencies]) => Array<Falsy | Validator<T, Err>>;
-}) => FieldValidator<T[], Err, Dependencies>;
+}) => FieldValidator<T, Err, Dependencies>;
 
 type FieldDescTuple<ValuesTuple extends readonly any[]> = {
   [Index in keyof ValuesTuple]: FieldDescriptor<ValuesTuple[Index]>;
