96db70900cc501d9bcf48f73b71f4a5eecf7e805
Viktoriya Bilovus <vbilovus@Vikas-MacBook-Pro-2.local>
2021-01-26 17:49:34 +0100

    feat: atom state in form controller draft

--- src/core/hooks/use-form-controller/formts-reducer.ts
+++ src/core/hooks/use-form-controller/formts-reducer.ts
@@ -1,6 +1,5 @@
-import { Reducer } from "react";
-
 import { filter, get, range, set } from "../../../utils";
+import { Atom } from "../../../utils/atoms";
 import {
   createInitialValues,
   makeTouchedValues,
@@ -9,132 +8,132 @@ import {
 import { FormtsOptions } from "../../types/formts-options";
 import { FormtsAction, FormtsState } from "../../types/formts-state";
 
-export const createReducer = <Values extends object, Err>(): Reducer<
-  FormtsState<Values, Err>,
-  FormtsAction<Values, Err>
-> => (state, action) => {
-  switch (action.type) {
-    case "reset": {
-      const { values } = action.payload;
-      const touched = makeUntouchedValues(values);
-
-      return {
-        values,
-        touched,
-        errors: {},
-        validating: {},
-        isSubmitting: false,
-      };
-    }
-
-    case "touchValue": {
-      const { path } = action.payload;
-
-      const value = get(state.values, path);
-      const touched = set(state.touched, path, makeTouchedValues(value));
-
-      return { ...state, touched };
-    }
-
-    case "setValue": {
-      const { path, value } = action.payload;
-
-      const resolveErrors = () => {
-        if (!Array.isArray(value)) {
-          return state.errors;
-        }
-
-        const currentValue = get(state.values, path) as unknown[];
-        if (currentValue.length <= value.length) {
-          return state.errors;
-        }
-
-        const hangingIndexes = range(value.length, currentValue.length - 1);
-        const errors = filter(
-          state.errors,
-          ({ key }) =>
-            !hangingIndexes.some(i => key.startsWith(`${path}[${i}]`))
-        );
-
-        return errors;
-      };
-
-      const values = set(state.values, path, value);
-      const touched = set(state.touched, path, makeTouchedValues(value));
-
-      return { ...state, values, touched, errors: resolveErrors() };
-    }
-
-    case "setErrors": {
-      const errors = action.payload.reduce(
-        (dict, { path, error }) => {
-          if (error != null) {
-            dict[path] = error;
-          } else {
-            delete dict[path];
-          }
-          return dict;
-        },
-        { ...state.errors }
-      );
-
-      return { ...state, errors };
-    }
-
-    case "validatingStart": {
-      const { path, uuid } = action.payload;
-
-      const validating = {
-        ...state.validating,
-        [path]: { ...state.validating[path], [uuid]: true as const },
-      };
-
-      return { ...state, validating };
-    }
-
-    case "validatingStop": {
-      const { path, uuid } = action.payload;
-
-      const validating = (() => {
-        if (state.validating[path] == null) {
-          return state.validating;
-        }
-
-        const validating = { ...state.validating };
-        const uuids = { ...validating[path] };
-        validating[path] = uuids;
-
-        delete uuids[uuid];
-
-        if (Object.keys(uuids).length === 0) {
-          delete validating[path];
-        }
-
-        return validating;
-      })();
-
-      return { ...state, validating };
-    }
-
-    case "setIsSubmitting": {
-      const { isSubmitting } = action.payload;
-      return { ...state, isSubmitting };
-    }
-  }
-};
-
 export const getInitialState = <Values extends object, Err>({
   Schema,
   initialValues,
-}: FormtsOptions<Values, Err>): FormtsState<Values, Err> => {
+}: FormtsOptions<Values, Err>): Atom<FormtsState<Values, Err>> => {
   const values = createInitialValues(Schema, initialValues);
   const touched = makeUntouchedValues(values);
 
-  return {
+  return Atom.of({
     values,
     touched,
     errors: {},
     validating: {},
     isSubmitting: false,
-  };
+  } as FormtsState<Values, Err>);
 };
+
+
+export const createStateDispatch = <Values extends object, Err>(stateAtom: Atom<FormtsState<Values, Err>>) =>
+  (action: FormtsAction<Values, Err>) => {
+    const state = stateAtom.val
+    switch (action.type) {
+      case "reset": {
+        const { values } = action.payload;
+        const touched = makeUntouchedValues(values);
+
+        return stateAtom.set({
+          values,
+          touched,
+          errors: {},
+          validating: {},
+          isSubmitting: false,
+        });
+      }
+
+      case "touchValue": {
+        const { path } = action.payload;
+
+        const value = get(state.values, path);
+        const touched = set(state.touched, path, makeTouchedValues(value));
+
+        return stateAtom.set({ ...state, touched });
+      }
+
+      case "setValue": {
+        const { path, value } = action.payload;
+
+        const resolveErrors = () => {
+          if (!Array.isArray(value)) {
+            return state.errors;
+          }
+
+          const currentValue = get(state.values, path) as unknown[];
+          if (currentValue.length <= value.length) {
+            return state.errors;
+          }
+
+          const hangingIndexes = range(value.length, currentValue.length - 1);
+          const errors = filter(
+            state.errors,
+            ({ key }) =>
+              !hangingIndexes.some(i => key.startsWith(`${path}[${i}]`))
+          );
+
+          return errors;
+        };
+
+        const values = set(state.values, path, value);
+        const touched = set(state.touched, path, makeTouchedValues(value));
+
+        return stateAtom.set({ ...state, values, touched, errors: resolveErrors() });
+      }
+
+      case "setErrors": {
+        const errors = action.payload.reduce(
+          (dict, { path, error }) => {
+            if (error != null) {
+              dict[path] = error;
+            } else {
+              delete dict[path];
+            }
+            return dict;
+          },
+          { ...state.errors }
+        );
+
+        return stateAtom.set({ ...state, errors });
+      }
+
+      case "validatingStart": {
+        const { path, uuid } = action.payload;
+
+        const validating = {
+          ...state.validating,
+          [path]: { ...state.validating[path], [uuid]: true as const },
+       };
+
+        return stateAtom.set({ ...state, validating });
+      }
+
+      case "validatingStop": {
+        const { path, uuid } = action.payload;
+
+        const validating = (() => {
+          if (state.validating[path] == null) {
+            return state.validating;
+          }
+
+          const validating = { ...state.validating };
+          const uuids = { ...validating[path] };
+          validating[path] = uuids;
+
+          delete uuids[uuid];
+
+          if (Object.keys(uuids).length === 0) {
+            delete validating[path];
+          }
+
+          return validating;
+        })();
+
+        return stateAtom.set({ ...state, validating });
+      }
+
+      case "setIsSubmitting": {
+        const { isSubmitting } = action.payload;
+        return stateAtom.set({ ...state, isSubmitting });
+      }
+    }
+  }
--- src/core/hooks/use-form-controller/use-form-controller.tsx
+++ src/core/hooks/use-form-controller/use-form-controller.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useCallback, useMemo } from "react";
 
 import {
   FormController,
@@ -8,7 +8,7 @@ import { FormtsOptions } from "../../types/formts-options";
 import { opaque } from "../../types/type-mapper-util";
 
 import { createFormtsMethods } from "./formts-methods";
-import { createReducer, getInitialState } from "./formts-reducer";
+import { createStateDispatch, getInitialState } from "./formts-reducer";
 
 /**
  * Hook that manages form state - should be used in main form component.
@@ -34,16 +34,13 @@ import { createReducer, getInitialState } from "./formts-reducer";
 export const useFormController = <Values extends object, Err>(
   options: FormtsOptions<Values, Err>
 ): FormController => {
-  const [state, dispatch] = React.useReducer(
-    createReducer<Values, Err>(),
-    options,
-    getInitialState
-  );
+  const state = useMemo(() => getInitialState(options), [])
+  const dispatch = useCallback(createStateDispatch(state), [state])
 
-  const methods = createFormtsMethods({ options, state, dispatch });
+  const methods = createFormtsMethods({ options, state: state.val, dispatch });
 
   const controller: _FormControllerImpl<Values, Err> = {
-    __ctx: { options, state, methods },
+    __ctx: { options, state: state.val, methods },
   };
 
   return opaque(controller);
