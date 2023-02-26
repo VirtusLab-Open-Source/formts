520cf3e1671fc0e6f986ecc0ca4531bd4fd09982
Mikolaj Klaman <mixvar@gmail.com>
2023-02-26 20:49:28 +0100

    docs: add jsdoc hinting how to work with FieldError objects

--- src/core/types/field-error.ts
+++ src/core/types/field-error.ts
@@ -1,4 +1,8 @@
 export type FieldError<Err> = {
+  /**
+   * ID string that enables connecting the error to a specific field.
+   * Use `FieldMatcher` helper class to compare it against FieldDescriptor objects from your form schema.
+   */
   fieldId: string;
   error: Err;
 };
